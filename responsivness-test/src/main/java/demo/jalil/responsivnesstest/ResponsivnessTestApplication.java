5ef933d2ad99a3dc5385d8aa23a95d0d686c8bf4
jarjanazy <celil.cercenazi@gmail.com>
2021-02-22 22:58:53 +0300

    init the spring project

---
+++ responsivness-test/src/main/java/demo/jalil/responsivnesstest/ResponsivnessTestApplication.java
@@ -0,0 +1,13 @@
+package demo.jalil.responsivnesstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponsivnessTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponsivnessTestApplication.class, args);
+	}
+
+}
---
+++ responsivness-test/src/test/java/demo/jalil/responsivnesstest/ResponsivnessTestApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.jalil.responsivnesstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponsivnessTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
