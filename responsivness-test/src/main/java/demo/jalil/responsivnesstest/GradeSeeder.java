1072f93f046106e071c4050e23ab88a12de5c76c
jarjanazy <celil.cercenazi@gmail.com>
2021-02-22 23:49:54 +0300

    Add 5 Grades to the database on startup

--- responsivness-test/src/main/java/demo/jalil/responsivnesstest/GradeSeeder.java
+++ responsivness-test/src/main/java/demo/jalil/responsivnesstest/GradeSeeder.java
@@ -5,6 +5,7 @@ import demo.jalil.responsivnesstest.grade.GradeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 
 @Component @RequiredArgsConstructor
 public class GradeSeeder implements CommandLineRunner {
@@ -12,8 +13,12 @@ public class GradeSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Grade grade = new Grade("1", 80.0, "Ahmad");
-
-        gradeRepository.save(grade).subscribe();
+        Flux.just(new Grade("1", 80.0, "Ahmad"),
+                new Grade("2", 90.0, "Ali"),
+                new Grade("3", 70.0, "Jalil"),
+                new Grade("4", 88.0, "Adnan"),
+                new Grade("5", 42.0, "Craig")).
+                flatMap(gradeRepository::save).// why flatMap, because save return a Mono. so if we use map we will end up with a Flux of Monos
+                subscribe();
     }
 }
