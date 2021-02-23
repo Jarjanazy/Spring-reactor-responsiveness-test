311ba8474bf4c0a70d7e6b7e376dfeee10c06157
jarjanazy <celil.cercenazi@gmail.com>
2021-02-23 21:21:44 +0300

    Add the cors policy to the grade controller

--- servlet-stack/src/main/java/com/example/demo/grade/GradeController.java
+++ servlet-stack/src/main/java/com/example/demo/grade/GradeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.grade;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,9 @@ public class GradeController {
     private final GradeRepository gradeRepository;
 
     @GetMapping("/grade/{id}")
-    public Grade getGradeById(@PathVariable String id){
+    @CrossOrigin(origins = "*")
+    public Grade getGradeById(@PathVariable String id) throws InterruptedException {
+        Thread.sleep(1000);
         return gradeRepository.
                 findById(Integer.parseInt(id)).
                 orElse(new Grade());
