23a023a4d599f0a593a9f80cde77446befc12c1d
jarjanazy <celil.cercenazi@gmail.com>
2021-02-23 16:02:18 +0300

    Make the client issue 1000 queries to the server

--- Client/client.js
+++ Client/client.js
@@ -1,11 +1,23 @@
+gradesToBeQuried = [1, 2, 3, 4, 5]
 
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = () => callback(xmlHttp.responseText);
+    xmlHttp.onload = () => callback(xmlHttp.responseText);
     xmlHttp.open("GET", theUrl, true); // true for asynchronous
     xmlHttp.send(null);
 }
 
+function getURLWithRandomGradeID(grades){
+    randomGradeId = grades[Math.floor(Math.random() * grades.length)];
+    return "http://localhost:8080/grade/" + randomGradeId;
+}
 
-httpGetAsync("http://localhost:8080/grade/1", (result) => console.log(result))
+function queryGrade(currentIndex){
+    let urlWithRandomGradeID = getURLWithRandomGradeID(gradesToBeQuried);
+    httpGetAsync(urlWithRandomGradeID, (result) => console.log(currentIndex + result))
+}
+
+Array(1000).fill().map((_, i) => queryGrade(i));
+console.log("DONE!!!!!")
+
