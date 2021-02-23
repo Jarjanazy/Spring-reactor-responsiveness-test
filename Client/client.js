d71cb1fc059e0f69fd55d2deb52fc646c7fd445b
jarjanazy <celil.cercenazi@gmail.com>
2021-02-23 14:10:13 +0300

    Add a configuration file for the CORS, add a dummy request to the client.js file

--- Client/client.js
+++ Client/client.js
@@ -1,1 +1,11 @@
-console.log("HEY")
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = () => callback(xmlHttp.responseText);
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
+}
+
+
+httpGetAsync("http://localhost:8080/grade/1", (result) => console.log(result))
