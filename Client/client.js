ca37fed7f570c5d010b15ec2241ae96b90d8f032
jarjanazy <celil.cercenazi@gmail.com>
2021-02-23 16:21:18 +0300

    add a condition to check and log the duration for total execution

--- Client/client.js
+++ Client/client.js
@@ -1,5 +1,7 @@
 gradesToBeQuried = [1, 2, 3, 4, 5]
 
+var t0 = performance.now()
+
 function httpGetAsync(theUrl, callback)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -13,11 +15,25 @@ function getURLWithRandomGradeID(grades){
     return "http://localhost:8080/grade/" + randomGradeId;
 }
 
-function queryGrade(currentIndex){
-    let urlWithRandomGradeID = getURLWithRandomGradeID(gradesToBeQuried);
-    httpGetAsync(urlWithRandomGradeID, (result) => console.log(currentIndex + result))
+function checkFinishConditionAndLog(currentIndex, totalNumberOfQueries) {
+    if (currentIndex === totalNumberOfQueries - 1) {
+        var t1 = performance.now()
+        console.log("it took " + (t1 - t0) / 1000 + " seconds.")
+    }
 }
 
-Array(1000).fill().map((_, i) => queryGrade(i));
-console.log("DONE!!!!!")
+function queryGrade(currentIndex, totalNumberOfQueries){
+    let urlWithRandomGradeID = getURLWithRandomGradeID(gradesToBeQuried);
+    httpGetAsync(urlWithRandomGradeID, (result) => {
+        console.log(currentIndex + result);
+        checkFinishConditionAndLog(currentIndex, totalNumberOfQueries);
+    })
+}
+
+function runQueries(){
+    const totalNumberOfQueries = 1000;
+    Array(totalNumberOfQueries).fill().map((_, i) => queryGrade(i, totalNumberOfQueries));
+}
+
+runQueries();
 
